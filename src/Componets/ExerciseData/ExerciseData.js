307c631db41e79300515b9ec45bfdca96b563baa
AndrewMedvedev42 <sanjino56@gmail.com>
2021-04-21 19:33:48 +0300

    quick bug fix

--- src/Componets/ExerciseData/ExerciseData.js
+++ src/Componets/ExerciseData/ExerciseData.js
@@ -8,18 +8,18 @@ const Data = [
     {id:5,  sortKey:"easy",problemDescription:"Norm ex" ,mathProblem:"6c+14=−5c+4+9c", answer:7},
 
 
-    {id:1, sortKey:"norm",problemDescription:"" ,mathProblem:"6c+14=−5c+4+9c", answer:7},
-    {id:2,  sortKey:"norm",problemDescription:"Norm ex" ,mathProblem:"6c+14=−5c+4+9c", answer:7},
-    {id:3,  sortKey:"norm",problemDescription:"Norm ex" ,mathProblem:"6c+14=−5c+4+9c", answer:7},
-    {id:4,  sortKey:"norm",problemDescription:"Norm ex" ,mathProblem:"6c+14=−5c+4+9c", answer:7},
-    {id:5,  sortKey:"norm",problemDescription:"Norm ex" ,mathProblem:"6c+14=−5c+4+9c", answer:7},
+    {id:11, sortKey:"norm",problemDescription:"" ,mathProblem:"6c+14=−5c+4+9c", answer:7},
+    {id:22,  sortKey:"norm",problemDescription:"Norm ex" ,mathProblem:"6c+14=−5c+4+9c", answer:7},
+    {id:33,  sortKey:"norm",problemDescription:"Norm ex" ,mathProblem:"6c+14=−5c+4+9c", answer:7},
+    {id:44,  sortKey:"norm",problemDescription:"Norm ex" ,mathProblem:"6c+14=−5c+4+9c", answer:7},
+    {id:55,  sortKey:"norm",problemDescription:"Norm ex" ,mathProblem:"6c+14=−5c+4+9c", answer:7},
 
 
-    {id:1, sortKey:"hard",problemDescription:"lol" ,mathProblem:"6c+14=−5c+4+9c", answer:7},
-    {id:2,  sortKey:"hard",problemDescription:"Hard ex" ,mathProblem:"6c+14=−5c+4+9c", answer:7},
-    {id:3,  sortKey:"hard",problemDescription:"Hard ex" ,mathProblem:"6c+14=−5c+4+9c", answer:7},
-    {id:4,  sortKey:"hard",problemDescription:"Hard ex" ,mathProblem:"6c+14=−5c+4+9c", answer:7},
-    {id:5,  sortKey:"hard",problemDescription:"Hard ex" ,mathProblem:"6c+14=−5c+4+9c", answer:7}
+    {id:111, sortKey:"hard",problemDescription:"lol" ,mathProblem:"6c+14=−5c+4+9c", answer:7},
+    {id:222,  sortKey:"hard",problemDescription:"Hard ex" ,mathProblem:"6c+14=−5c+4+9c", answer:7},
+    {id:333,  sortKey:"hard",problemDescription:"Hard ex" ,mathProblem:"6c+14=−5c+4+9c", answer:7},
+    {id:444,  sortKey:"hard",problemDescription:"Hard ex" ,mathProblem:"6c+14=−5c+4+9c", answer:7},
+    {id:555,  sortKey:"hard",problemDescription:"Hard ex" ,mathProblem:"6c+14=−5c+4+9c", answer:7}
 ]
 
 //Exporting the data
