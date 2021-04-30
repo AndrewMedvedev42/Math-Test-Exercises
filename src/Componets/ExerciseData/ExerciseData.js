25bb3172a9244b8e6ce25ac1fa16953dab18c250
AndrewMedvedev42 <sanjino56@gmail.com>
2021-04-30 16:58:15 +0300

    added content to about section

--- src/Componets/ExerciseData/ExerciseData.js
+++ src/Componets/ExerciseData/ExerciseData.js
@@ -9,7 +9,7 @@ const Data = [
     {id:6,  sortKey:"easy",problemDescription:"Norm ex" ,mathProblem:"12x + 0 = 144", answer:12},
 
 
-    {id:11, sortKey:"medium",problemDescription:"Mario answered 30% of the questions correctly. The test contained a total of 80 questions. How many questions did Mario answer correctly?" ,mathProblem:"", answer:7},
+    {id:11, sortKey:"medium",problemDescription:"Mario answered 30% of the questions correctly. The test contained a total of 80 questions. How many questions did Mario answer uncorrectly?" ,mathProblem:"", answer:7},
     {id:22,  sortKey:"medium",problemDescription:"Medium" ,mathProblem:"6c+14=c", answer:7},
     {id:33,  sortKey:"medium",problemDescription:"Medium" ,mathProblem:"65+9c", answer:7},
     {id:44,  sortKey:"medium",problemDescription:"Medium" ,mathProblem:"6c5c+4+9c", answer:7},
--- src/Componets/Navigation/AboutSection.js
+++ src/Componets/Navigation/AboutSection.js
@@ -1,5 +1,14 @@
+import '../../css/About.css';
+
 function About() {
-    return <h2>About</h2>;
+    return (
+      <section className="abouttitlesect">
+        <h1>About</h1>
+        <div>
+          <p>Math-Test-Exersices is non commersial platform for solving easy/medium/hard algebraic problems</p>
+        </div>
+      </section>
+    )
   }
 
 export default About
