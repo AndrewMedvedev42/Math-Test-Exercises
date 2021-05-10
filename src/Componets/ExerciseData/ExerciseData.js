604cf3c2c1ee43d72bfa058a2f7b05bfd800a619
AndrewMedvedev42 <sanjino56@gmail.com>
2021-05-10 20:25:38 +0300

    added questions to medium section

--- src/Componets/ExerciseData/ExerciseData.js
+++ src/Componets/ExerciseData/ExerciseData.js
@@ -9,12 +9,12 @@ const Data = [
     {id:6,  sortKey:"easy",problemDescription:"Find for X" ,mathProblem:"-10x - 19 = 19 - 8x ", answer:-19},
 
 
-    {id:11, sortKey:"medium",problemDescription:"Evaluate" ,mathProblem:"2+2^2", answer:1},
-    {id:22,  sortKey:"medium",problemDescription:"Evaluate" ,mathProblem:"2+2^2", answer:1},
-    {id:33,  sortKey:"medium",problemDescription:"Evaluate" ,mathProblem:"2+2^2", answer:1},
-    {id:44,  sortKey:"medium",problemDescription:"Evaluate" ,mathProblem:"2+2^2", answer:4},
-    {id:55,  sortKey:"medium",problemDescription:"Evaluate" ,mathProblem:"2+2^2", answer:5},
-    {id:66,  sortKey:"medium",problemDescription:"Evaluate" ,mathProblem:"2+2^2", answer:1},
+    {id:11, sortKey:"medium",problemDescription:"Evaluate, when X = 10" ,mathProblem:" 30 - |-x + 6|", answer:26},
+    {id:22,  sortKey:"medium",problemDescription:"Evaluate when X = 2 and Y = -2" ,mathProblem:"2xy^3 + x - 2y", answer:1},
+    {id:33,  sortKey:"medium",problemDescription:"How many solutions does the equation 2x2 + 3x = 8 have?" ,mathProblem:"", answer:2},
+    {id:44,  sortKey:"medium",problemDescription:"Evaluate" ,mathProblem:"11+\\sqrt{-4 + 6×4÷3}", answer:13},
+    {id:55,  sortKey:"medium",problemDescription:"Solve the equation" ,mathProblem:"-0.25 x + 1.3 = -0.55 x - 0.2", answer:-5},
+    {id:66,  sortKey:"medium",problemDescription:"What is the slope of a line perpendicular to the line x = -3?" ,mathProblem:"", answer:0},
 
 
     {id:21, sortKey:"hard",problemDescription:"Hard" ,mathProblem:"2+2^2", answer:1},
