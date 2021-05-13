fa81f0acfbadf79704bd4795b2ecb334b2094a65
AndrewMedvedev42 <sanjino56@gmail.com>
2021-05-13 19:24:30 +0300

    added new tips

--- src/Componets/Navigation/AboutSection.js
+++ src/Componets/Navigation/AboutSection.js
@@ -3,10 +3,14 @@ import '../../css/About.css';
 function About() {
     return (
       <section className="abouttitlesect">
-        <h1>About</h1>
         <div>
+          <h1>About</h1>
           <p>Math-Test-Exersices is non commersial platform for solving easy/medium/hard algebraic problems</p>
           <p>For now, this website does not contain hard stuctured exersises, and will be added later.</p>
+          <h1>How to fill answers:</h1>
+          <p>1. Answers should not contain spaces (write correctly: -2).</p>
+          <p>2. Ordinary fractions - through a slash (correct to write: 4/3);</p>
+          <p>3. Decimal fractions - through a dot without spaces (write correctly: 4.7)</p>
         </div>
       </section>
     )
