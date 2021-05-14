e38731f63df02af638400cea269e1cc4ed9c20cf
AndrewMedvedev42 <sanjino56@gmail.com>
2021-05-14 21:00:08 +0300

    added exercises to hard section, made submit buttons more interactive

--- src/Componets/ExerciseData/ExerciseData.js
+++ src/Componets/ExerciseData/ExerciseData.js
@@ -1,28 +1,28 @@
 //Here is the data that cotains description, math problem, and answer
 
 const Data = [
-    {id:1, sortKey:"easy", problemDescription:"Find for X"   ,mathProblem:"7x - 6 = 22", answer:4},
-    {id:2, sortKey:"easy",problemDescription:"Find for X" ,mathProblem:"5 + 2x = 15", answer:5},
-    {id:3, sortKey:"easy",problemDescription:"Find for X" ,mathProblem:"4x + 3 = 11", answer:2},
-    {id:4,  sortKey:"easy",problemDescription:"Find for X" ,mathProblem:"-5x + 20 = 25", answer:-1},
-    {id:5,  sortKey:"easy",problemDescription:"Find for X" ,mathProblem:"12x + 0 = 144", answer:12},
-    {id:6,  sortKey:"easy",problemDescription:"Find for X" ,mathProblem:"-10x - 19 = 19 - 8x ", answer:-19},
+    {id:1, sortKey:"easy", problemDescription:"Find for X:"   ,mathProblem:"7x - 6 = 22", answer:4},
+    {id:2, sortKey:"easy",problemDescription:"Find for X:" ,mathProblem:"5 + 2x = 15", answer:5},
+    {id:3, sortKey:"easy",problemDescription:"Find for X:" ,mathProblem:"4x + 3 = 11", answer:2},
+    {id:4,  sortKey:"easy",problemDescription:"Find for X:" ,mathProblem:"-5x + 20 = 25", answer:-1},
+    {id:5,  sortKey:"easy",problemDescription:"Find for X:" ,mathProblem:"12x + 0 = 144", answer:12},
+    {id:6,  sortKey:"easy",problemDescription:"Find for X:" ,mathProblem:"-10x - 19 = 19 - 8x ", answer:-19},
 
 
-    {id:11, sortKey:"medium",problemDescription:"Evaluate, when X = 10" ,mathProblem:" 30 - |-x + 6|", answer:26},
-    {id:22,  sortKey:"medium",problemDescription:"Evaluate when X = 2 and Y = -2" ,mathProblem:"2xy^3 + x - 2y", answer:1},
+    {id:11, sortKey:"medium",problemDescription:"Evaluate, when X = 10:" ,mathProblem:" 30 - |-x + 6|", answer:26},
+    {id:22,  sortKey:"medium",problemDescription:"Evaluate when X = 2 and Y = -2:" ,mathProblem:"2xy^3 + x - 2y", answer:1},
     {id:33,  sortKey:"medium",problemDescription:"How many solutions does the equation 2x2 + 3x = 8 have?" ,mathProblem:"", answer:2},
-    {id:44,  sortKey:"medium",problemDescription:"Evaluate" ,mathProblem:"11+\\sqrt{-4 + 6×4÷3}", answer:13},
-    {id:55,  sortKey:"medium",problemDescription:"Solve the equation" ,mathProblem:"-0.25 x + 1.3 = -0.55 x - 0.2", answer:-5},
+    {id:44,  sortKey:"medium",problemDescription:"Evaluate:" ,mathProblem:"11+\\sqrt{-4 + 6×4÷3}", answer:13},
+    {id:55,  sortKey:"medium",problemDescription:"Solve the equation:" ,mathProblem:"-0.25 x + 1.3 = -0.55 x - 0.2", answer:-5},
     {id:66,  sortKey:"medium",problemDescription:"What is the slope of a line perpendicular to the line x = -3?" ,mathProblem:"", answer:0},
 
 
-    {id:21, sortKey:"hard",problemDescription:"Hard" ,mathProblem:"2+2^2", answer:1},
-    {id:22,  sortKey:"hard",problemDescription:"Hard" ,mathProblem:"2+2^2", answer:1},
-    {id:23,  sortKey:"hard",problemDescription:"Hard" ,mathProblem:"2+2^2", answer:1},
-    {id:24,  sortKey:"hard",problemDescription:"Hard" ,mathProblem:"2+2^2", answer:1},
-    {id:25,  sortKey:"hard",problemDescription:"Hard" ,mathProblem:"2+2^2", answer:1},
-    {id:26,  sortKey:"hard",problemDescription:"Hard" ,mathProblem:"2+2^2", answer:1}
+    {id:21, sortKey:"hard",problemDescription:"What is the number of solutions of this eequation? (The answer should be a number)" ,mathProblem:"(x^2 + 1)^2 + 2(x^2 + 1) - 3 = 0", answer:1},
+    {id:22,  sortKey:"hard",problemDescription:"For all x real:" ,mathProblem:"\\sqrt{(x2 -4x + 4)}", answer:"|x-2|"},
+    {id:23,  sortKey:"hard",problemDescription:"" ,mathProblem:"\\sqrt{(x 2 + 6x + 9)} =", answer:"|x+3|"},
+    {id:24,  sortKey:"hard",problemDescription:"Solve the equation:" ,mathProblem:"2 (4^{x+1}) = 4 (8^{x+2})", answer:-5},
+    {id:25,  sortKey:"hard",problemDescription:"How many solutions do the equation have? (The answer should be a number)" ,mathProblem:"1/x + 1/(x + 1) = 1/3", answer:2},
+    {id:26,  sortKey:"hard",problemDescription:"Simplify:" ,mathProblem:"9\\log _9(4)", answer:4}
 ]
 
 //Exporting the data
--- src/Componets/Navigation/AboutSection.js
+++ src/Componets/Navigation/AboutSection.js
@@ -11,6 +11,7 @@ function About() {
           <p>1. Answers should not contain spaces (write correctly: -2).</p>
           <p>2. Ordinary fractions - through a slash (correct to write: 4/3);</p>
           <p>3. Decimal fractions - through a dot without spaces (write correctly: 4.7)</p>
+          <h1>Happy learning :)</h1>
         </div>
       </section>
     )
